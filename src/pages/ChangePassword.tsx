f3303ac3325545b100beed8294c4a2eb0de87faf
Amaechi747 <amaechimoses8@gmail.com>
2022-08-13 14:15:31 +0100

    Setup and Structured files in the pages folder

---
+++ src/pages/ChangePassword.tsx
@@ -0,0 +1,11 @@
+import React, {FC} from 'react'
+
+type Props = {}
+
+const ChangePassword: React.FC = (props: Props) => {
+  return (
+    <div>ChangePassword</div>
+  )
+}
+
+export default ChangePassword
---
+++ src/pages/SuperAdminSignUp.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const SuperAdminSignUp = (props: Props) => {
+  return (
+    <div>SuperAdminSignUp</div>
+  )
+}
+
+export default SuperAdminSignUp
