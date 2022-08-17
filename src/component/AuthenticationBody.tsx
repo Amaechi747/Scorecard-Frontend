4a9fa00266057d7cc0e914d315507dd3ededc598
Yama Neezah <YamaNeezah@gmail.com>
2022-08-17 01:54:22 +0100

    Login 90%

---
+++ src/component/AuthenticationBody.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import { LoginHeading } from '../styling/css';
+import AuthenticationLeft from "./AuthenticationLeft";
+import AuthenticationRight from "./AuthenticationRight";
+import ScorecardLogo from './ScorecardLogo';
+import { Link } from "react-router-dom";
+import AuthenticationButton from './AuthenticationButton';
+
+
+interface IAuthenticationBodyProps {
+}
+
+const AuthenticationBody: React.FunctionComponent<IAuthenticationBodyProps> = (props) => {
+    return (
+        <>
+            <div style={{ display: 'flex', backgroundColor: '#fff' }}>
+                <div style={{ flex: '1', margin: "3rem 5rem" }}>
+                    <ScorecardLogo />
+                    <LoginHeading>Login to your account</LoginHeading>
+                    <AuthenticationLeft />
+                    <p style={{paddingLeft: "25rem" }}><Link to="/forget-password" style={{textDecoration: "none", color: "#34A853"}}>Forgot Password?</Link></p>
+                    <AuthenticationButton text="Login" />
+                    <p style={{textAlign: "center"}}>Don't have an account? <Link to="/sign-up" style={{textDecoration: "none", color: "#34A853"}}>Sign Up</Link></p>
+                </div>
+                <div style={{ flex: "1" }}>
+                    <AuthenticationRight />
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default AuthenticationBody;
--- src/component/FormButton.tsx
+++ src/component/FormButton.tsx
@@ -1,21 +1,11 @@
 import React from 'react'
-import styled from "styled-components"
+import { ButtonBody, ButtonText } from "../styling/css"
 
 type ButtonProps = {
   text: string
 }
 
-const ButtonBody = styled.button`
-width: calc(100% - 0.21rem);
-height: 2.5rem;
-border: 1px solid rgba(33, 51, 79, 0.15);
-margin: 0.4rem 0;
-background: #14A800;
-`
 
-const ButtonText = styled.p`
-color: #FFFFFF;
-`
 
 const FormButton = (props: ButtonProps) => {
   const { text } = props
