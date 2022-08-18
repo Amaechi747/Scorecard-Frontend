b068868a4aeb86920ecc7e2e5a718887767c4005
Yama Neezah <YamaNeezah@gmail.com>
2022-08-18 15:36:21 +0100

    login screen

--- src/component/ScorecardLogo.tsx
+++ src/component/ScorecardLogo.tsx
@@ -9,9 +9,11 @@ const ScorecardLogo: React.FunctionComponent<IScorecardLogoProps> = (props) => {
   return (
     <>
     <div style={{display: "flex", marginBottom: '3rem'}}>
+      
         <div style={{flex: "0.3"}}>
             <img src={logo} alt="" />
         </div>
+
         <div style={{flex: "0.1", margin: "0.5rem 0 0.4rem 0rem"}}>
             <ScoreCardHeading>Scorecard</ScoreCardHeading>
 
