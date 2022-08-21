2eadf23bab102a6925efab6c5ac96fb3ec1d5bc1
rex-jay <dec@Decagons-MacBook-Air-2.local>
2022-08-21 23:04:54 +0100

    changes to superadmin.tsx

--- src/pages/SuperAdminSignUp.tsx
+++ src/pages/SuperAdminSignUp.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState, ChangeEvent, FormEvent } from "react";
+import React, { FC, useState, useEffect, ChangeEvent, FormEvent } from "react";
 import { FormInput, Card, FormButton, Form } from "../component";
 import { Heading, Paragraph, HorizontalLine } from "../styling/css";
 import axios from "axios"
@@ -13,6 +13,8 @@ const SuperAdminSignUp: FC = (props: SuperAdminSignUpProps) => {
     email: "",
     password: "",
   });
+  const [ formErrors, setFormErrors] = useState({firstName:'', lastName:'', email: '', password: ''});
+  const [isSubmit, setIsSubmit] = useState(false);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setFormData({
@@ -23,7 +25,9 @@ const SuperAdminSignUp: FC = (props: SuperAdminSignUpProps) => {
 
   const handleSubmit = async(e: FormEvent<HTMLFormElement>) => {
     try {
-      e.preventDefault()
+      e.preventDefault();
+      setFormErrors(validate(formData));
+      setIsSubmit(true);
       await axios.post("http://localhost:5000/admin/superuser",formData)
       swal("Success", "You have successfully signed up!", "success");
 
@@ -31,6 +35,35 @@ const SuperAdminSignUp: FC = (props: SuperAdminSignUpProps) => {
         swal("Error", "Something went wrong", "error");
     }
   }
+
+  useEffect(() => {
+    console.log(formErrors);
+    if(Object.keys(formErrors).length === 0 && isSubmit){
+      console.log(formData);
+    }
+  }, [formErrors]);
+
+  const validate = (values: any) => {
+    const errors = {firstName:'', lastName:'', email: '', password: ''};
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    if(!values.firstName){
+      errors.firstName = "First name is required";
+    }
+    if(!values.lastName){
+      errors.lastName = "Last name is required";
+    }
+    if(!values.email){
+      errors.email = "Email is required";
+    } else if(!regex.test(values.email)){
+      errors.email = "This is not a valid email address";
+    }
+    if(!values.password){
+      errors.password = "Password is required";
+    } else if(values.password.length < 4){
+      errors.password = "Password must be at least 4 characters";
+    }
+    return errors;
+  };
 
   return (
     <>
@@ -52,6 +85,7 @@ const SuperAdminSignUp: FC = (props: SuperAdminSignUpProps) => {
               name="firstName"
               onChange={(e) => handleChange(e)}
             />
+            <p>{formErrors.firstName}</p>
 
             <FormInput 
               label="Last Name"
@@ -60,6 +94,7 @@ const SuperAdminSignUp: FC = (props: SuperAdminSignUpProps) => {
               name="lastName"
               onChange={(e) => handleChange(e)}
             />
+            <p>{formErrors.lastName}</p>
 
             <FormInput 
               label="Email"
@@ -68,6 +103,7 @@ const SuperAdminSignUp: FC = (props: SuperAdminSignUpProps) => {
               name="email"
               onChange={(e) => handleChange(e)}
             />
+            <p>{formErrors.email}</p>
 
             <FormInput 
               label="Password"
@@ -76,6 +112,7 @@ const SuperAdminSignUp: FC = (props: SuperAdminSignUpProps) => {
               name="password"
               onChange={(e) => handleChange(e)}
             />
+            <p>{formErrors.password}</p>
 
             <FormButton text="Create Super Admin" />
 
