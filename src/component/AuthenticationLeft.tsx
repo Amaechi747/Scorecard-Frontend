1673a10498fccd3fc6458134609165e98dab0a9a
Yama Neezah <YamaNeezah@gmail.com>
2022-08-26 00:55:07 +0100

    update features

--- src/component/AuthenticationLeft.tsx
+++ src/component/AuthenticationLeft.tsx
@@ -4,16 +4,11 @@ import { Link, useNavigate } from 'react-router-dom';
 import swal from 'sweetalert';
 import validator from 'validator';
 // import { AuthenticationInput, AuthenticationButton } from '.';
-import { AuthInput, AuthButton } from '.'
-
-
+import {AuthInput, AuthButton} from '.'
 type IAuthenticationLeftProps = {
 }
-
 const BASEURL = process.env.REACT_APP_BASEURL;
-
 const AuthenticationLeft = (props: IAuthenticationLeftProps) => {
-
     const [formData, setFormData] = useState({
         email: '',
         password: ''
@@ -22,79 +17,65 @@ const AuthenticationLeft = (props: IAuthenticationLeftProps) => {
     const [pass, setPass] = useState('');
     const navigate = useNavigate();
     // const [errMsg, setErrMSg] = useState('')
-
     // const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     //     const { name, value } = e.target
     //     setFormData({ ...formData, [name]: value })
-
     //     formData.email.length === 0 ? setErrMSg('Email cannot be empty') :
     //     formData.email.length > 0 && validator.isEmail(formData.email) ? setErrMSg('Valid Email') : setErrMSg('Please enter a valid email address')
     // }
     useEffect(() => {
-        if (mail || pass) {
-            setFormData({ email: mail, password: pass });
+        if(mail || pass) {
+            setFormData({ email: mail, password: pass});
         }
         return;
-    }, [mail, pass])
-
-    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    }, [mail,pass])
+    const handleSubmit = async(e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        try {
-
+        try{
             const response = await axios.post(`${BASEURL}/users/login`, formData);
-
-            response.data.message === 'Success' ? swal("Success", "Login Successful", "success") : void 0;
-            for (let item in response.data.data) {
+            response.data.message === 'Success' ? swal("Success","Login Successful", "success") : void 0;
+            for(let item in response.data.data) {
                 localStorage.setItem(item, response.data.data[item]);
-                if (item === 'admin') {
+                if(item === 'admin') {
                     Object.entries(response.data.data[item]).forEach(([key, value]) => {
-                        localStorage.setItem(key, `${value}`)
+                        localStorage.setItem(key, `${value}`);
                     });
-                    setTimeout(() => {
-                        navigate('/admin');
+                    setTimeout(()=>{
+                        navigate('/admin-dashboard/profile');
                     }, 900)
                 }
             }
-            setTimeout(() => {
-                navigate('/admin-dashboard/profile');
-            }, 900)
-
-        } catch (err: any) {
-            if (err?.response.data.error) {
+            // localStorage.setItem
+        }catch(err: any) {
+            if(err?.response.data.error) {
                 const message = err?.response.data.error
-                swal("Error", message, "error")
+                swal("Error",message, "error")
             }
         }
-
-        return (
-            <>
-                <form onSubmit={(e) => handleSubmit(e)}>
-                    <AuthInput name='email' placeholder='Email Address' label='Email Address'
-                        type='email' errorMsg='Please enter a valid email address' setSharedState={setMail} />
-
-
-                    <div style={{ display: 'flex', flexDirection: 'column', marginBottom: '3rem' }}>
-                        <AuthInput
-                            label="Password"
-                            type="password"
-                            name="password"
-                            placeholder="Password"
-                            errorMsg='Please input a password with letters, numbers and symbols @_:;'
-                            setSharedState={setPass}
-                        />
-                        <Link to="/forgot-password" style={{
-                            textDecoration: "none",
-                            color: "#34A853",
-                            alignSelf: 'flex-end',
-                            marginTop: '-1.2rem'
-                        }}>Forgot Password?</Link>
-                    </div>
-
-
-                    <AuthButton text="Login" />
-                </form>
-            </>
-        );
-    };
-
-    export default AuthenticationLeft;
+    }
+    return (
+        <>
+            <form onSubmit={(e)=> handleSubmit(e)}>
+                <AuthInput name='email' placeholder='Email Address' label='Email Address'
+                type='email' errorMsg='Please enter a valid email address' setSharedState={setMail} />
+                <div style={{ display: 'flex', flexDirection: 'column', marginBottom: '3rem' }}>
+                    <AuthInput
+                        label="Password"
+                        type="password"
+                        name="password"
+                        placeholder="Password"
+                        errorMsg='Please input a password with letters, numbers and symbols @_:;'
+                        setSharedState={setPass}
+                    />
+                    <Link to="/forgot-password" style={{
+                        textDecoration: "none", 
+                        color: "#34A853",
+                        alignSelf: 'flex-end',
+                        marginTop: '-1.2rem'}}>Forgot Password?</Link>
+                </div>
+                <AuthButton text="Login" />
+            </form>
+        </>
+    );
+};
+export default AuthenticationLeft;
